acddc104dbfcf2cb8638a196402e2662aba68078
Harrison Deng <yunyangdeng@gmail.com>
2023-12-09 07:40:57 +0000

    Added result checking for matrix pairwise breakdown test.

--- tests/testthat/test-SNVAnalysis.R
+++ tests/testthat/test-SNVAnalysis.R
@@ -14,6 +14,20 @@ testthat::test_that("pairwise table generation produces correct permutations of 
     )
   )
   testthat::expect_type(results, "list")
+  testthat::expect_equal(
+    results,
+    list(
+      matrix(data = c(1,2,4,5), nrow = 2, ncol = 2, byrow = TRUE),
+      matrix(data = c(1,2,7,8), nrow = 2, ncol = 2, byrow = TRUE),
+      matrix(data = c(4,5,7,8), nrow = 2, ncol = 2, byrow = TRUE),
+      matrix(data = c(1,3,4,6), nrow = 2, ncol = 2, byrow = TRUE),
+      matrix(data = c(1,3,7,9), nrow = 2, ncol = 2, byrow = TRUE),
+      matrix(data = c(4,6,7,9), nrow = 2, ncol = 2, byrow = TRUE),
+      matrix(data = c(2,3,5,6), nrow = 2, ncol = 2, byrow = TRUE),
+      matrix(data = c(2,3,8,9), nrow = 2, ncol = 2, byrow = TRUE),
+      matrix(data = c(5,6,8,9), nrow = 2, ncol = 2, byrow = TRUE)
+    )
+  )
 })
 
 test_that("case control with ensembl database as control produces expected table", {
@@ -22,5 +36,5 @@ test_that("case control with ensembl database as control produces expected table
     RsIDs,
     totalCaseSamples = 8
   )
-  expect_type(results, data.frame)
+  expect_type(results, "data.frame")
 })
